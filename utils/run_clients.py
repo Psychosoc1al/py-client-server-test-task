0964f29d5536778c65c20a8637c3518462f90d0b
Psychosoc1al <tohichpetrov@gmail.com>
2024-05-25 03:30:03 +0300

    Add testing scripts to run in parallel and generate files for sending

---
+++ utils/run_clients.py
@@ -0,0 +1,33 @@
+import multiprocessing
+import os
+import subprocess
+
+
+def run(
+    script_path: str,
+    file_path: str,
+    host: str,
+    port: int,
+) -> None:
+    result = subprocess.run(
+        ["python", script_path, file_path, host, str(port)],
+        capture_output=True,
+        text=True,
+    )
+    print(f"STDOUT: {result.stdout}")
+    print(f"STDERR: {result.stderr}")
+
+
+def main() -> None:
+    runs_num = 6
+    script_path = os.path.abspath("../client.py")
+    file_path = "../test_files/test_file_100000.txt"
+    host = "127.0.0.1"
+    port = 12345
+
+    with multiprocessing.Pool(processes=runs_num) as pool:
+        pool.starmap(run, [(script_path, file_path, host, port)] * runs_num)
+
+
+if __name__ == "__main__":
+    main()
